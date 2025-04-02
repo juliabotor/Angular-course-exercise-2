b416046b63ae6ca52b2c4bb85cbbb6c562c8ad14
Julia Boto <julia.boto.rodrigues@gmail.com>
2025-04-02 17:53:03 -0300

    Segundo comit

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,11 +7,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent implements OnInit {
+export class ServerStatusComponent implements OnInit{
   currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  private destroyRef = inject(DestroyRef);
 
-  constructor(){
-    setInterval(() => {
+  constructor(){}
+
+  ngOnInit(){
+    console.log('ON INIT');
+    const interval = setInterval(() => {
       const rnd = Math.random();
 
       if (rnd <0.5){
@@ -22,9 +26,14 @@ export class ServerStatusComponent implements OnInit {
         this.currentStatus = 'unknown';
       }
     }, 5000);
+
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval);
+    })
+  }
+  
+  ngAfterViewInit(){
+    console.log('AFTER VIEW INIT');
   }
 
-  ngOnInit(){
-
-  }
 }
